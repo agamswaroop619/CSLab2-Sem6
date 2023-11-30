f264fdd6852d3353fcef36fba1613ffc1e14a8f7
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-01 00:19:43 +0530

    add: mergesort

--- src/merge.java
+++ src/merge.java
@@ -1,2 +1,116 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class merge {
+    public static void main(String[] args) {
+        String filePath = "random_numbers.txt";
+        int[] arr = readNumbersFromFile(filePath);
+        long startTime = System.currentTimeMillis();
+
+        if (arr != null) {
+            System.out.println("Original Array:");
+            printArray(arr);
+            mergeSort(arr, 0, arr.length - 1);
+            System.out.println("\nSorted Array:");
+            printArray(arr);
+        } else {
+            System.out.println("Error reading numbers from file.");
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Execution Time: " + (endTime - startTime) + " milliseconds");
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int middle = left + (right - left) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, middle, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; ++i) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArray[j] = arr[middle + 1 + j];
+        }
+
+        // Merge the temporary arrays
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of leftArray[], if there are any
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray[], if there are any
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] readNumbersFromFile(String filePath) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line = reader.readLine();
+            int count = 0;
+
+            while (line != null) {
+                count++;
+                line = reader.readLine();
+            }
+
+            int[] arr = new int[count];
+
+            reader.close();
+            reader = new BufferedReader(new FileReader(filePath));
+
+            for (int i = 0; i < count; i++) {
+                line = reader.readLine();
+                arr[i] = Integer.parseInt(line);
+            }
+
+            reader.close();
+            return arr;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
